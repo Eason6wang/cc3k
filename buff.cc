b33b343b099622169cfe3296043eac56c90b7381
j765wang <j765wang@nettop1.student.cs.uwaterloo.ca>
2016-12-01 19:20:36 -0500

    make template

--- buff.cc
+++ buff.cc
@@ -1,5 +1,13 @@
 #include "buff.h"
 #include "object.h"
+#include"player.h"
+#include"enemy.h"
+#include <string>
+#include "character.h"
+#include "playerinfo.h"
+#include "type.h"
+#include "style.h"
+
 
 using namespace std;
 void clearScreen(){
@@ -32,4 +40,42 @@ bool compare(shared_ptr<Object> ob1, shared_ptr<Object> ob2){
 	} else {
 		return false;
 	}
+}
+
+
+//template
+template<typename Visit, typename Bevisit, typename Type>
+bool be_visit_by(Visit & v1, Bevisit & v2, Type t){
+    if(t == ATTACK){
+	return be_attack(v1, v2);
+    }
+    return false;
+}
+
+
+
+bool be_attack(Player &player, Enemy &enemy){
+    int damage = getDamage(enemy.getInfo().atk, player.getInfo().def);
+    player.getInfo().hp -= damage;
+    std::string newAction = getString(enemy.getPos().style) + 
+           " deals (" + std::to_string(damage) + ") damage to PC. ";
+    player.getPlayerInfo().action += newAction;
+    if(player.getInfo().hp <= 0){
+	throw true;
+    } else {
+	return true;
+    }
+}
+
+bool be_attack(Enemy &enemy, Player &player){  
+  int damage = getDamage(player.getInfo().atk, enemy.getInfo().def);
+    enemy.getInfo().hp -= damage;
+    std::string newAction = "PC deals (" + std::to_string(damage) +
+               ") damage to " + getString(enemy.getPos().style)  + ". ";
+    player.getPlayerInfo().action += newAction;
+    if(enemy.getInfo().hp <= 0){
+	//visitexcept
+    } else {
+	return true;
+    }
 }
--- buff.h
+++ buff.h
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <memory>
 class Object;
+class Player;
+class Enemy;
+
 void clearScreen();
 
 int getRandom(int min, int max);
@@ -13,4 +16,14 @@ int getDamage(int, int);
 
 bool compare(std::shared_ptr<Object> ob1,std::shared_ptr<Object> ob2);
 
+
+//template
+template<typename Visit, typename Bevisit, typename Type>
+bool be_visit_by(Visit & v1, Bevisit & v2, Type t);
+
+
+
+bool be_attack(Player &player, Enemy &enemy);
+
+bool be_attack(Enemy &enemy, Player &player);  
 #endif
