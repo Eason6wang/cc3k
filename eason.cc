a3a91307d453d5aecc6a5d61717a6832bef0bde9
j765wang <j765wang@nettop1.student.cs.uwaterloo.ca>
2016-12-03 23:47:25 -0500

    getback

---
+++ eason.cc
@@ -0,0 +1,12 @@
+#include"eason.h"
+#include "type.h"
+
+// nightelf can be invisible
+Eason::Eason(int row, int col):
+  Player{111, 29, 17, row, col, EASON}{}
+
+bool Eason::visit (Object &object, Type type) {
+    skill();
+	return object.be_visit (*this, type);
+}
+
